9e2edc5f2238f2bc798e2dcf454df76efda4e7f2
Gustavo_Oliveira_M <102737167+Mizuicy@users.noreply.github.com>
2023-03-08 16:22:19 -0300

    Delete api-de-eventos/src/main/java/com/starttech/api/de/eventos directory

--- api-de-eventos/src/main/java/com/starttech/api/de/eventos/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.starttech.api.de.eventos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventoRepository extends JpaRepository<Evento, Long> {
-
-}
