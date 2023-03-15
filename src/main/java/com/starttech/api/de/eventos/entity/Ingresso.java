8f08dd0d9eded65e67dac43e17433fdc6bdef80c
CTT <gustavosilva90@gmail.com>
2023-03-15 15:15:31 -0300

    Correção de erros para rodar a api

--- src/main/java/com/starttech/api/de/eventos/entity/Ingresso.java
+++ src/main/java/com/starttech/api/de/eventos/entity/Ingresso.java
@@ -1,6 +1,9 @@
 package com.starttech.api.de.eventos.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +11,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Ingresso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String nome;
